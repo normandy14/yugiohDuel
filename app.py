565f860b0cc209cd7a0e0ed44a2495e6728af387
normandy14 <sturdyprogrammer@gmail.com>
2023-03-25 17:44:06 -0400

    Create deck/<pro> route that retrieves info from yugioh api

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ def hello():
 
 @app.route("/deck/<pro>")
 def deck(pro):
-    card = yugioh.get_card("The Wicked Dreadroot") #Accepts both name and ID
-    print(card.name) #Returns "The Wicked Dreadroot"
-    print(card.archetype) #Returns "Wicked God"
-    print(card.attack) #Returns "4000"
+    card = yugioh.get_card("Mirror Force") #Accepts both name and ID
+    if card.type != "Spell Card" and card.type != "Trap Card":
+        print(card.name) #Returns "The Wicked Dreadroot"
+        print(card.archetype) #Returns "Wicked God"
+        print(card.attack) #Returns "4000"
+    else:
+        print(card.name)
+        print(card.description)
     url = "https://static-7.studiobebop.net/ygo_data/card_images/{}.jpg".format(pro.capitalize())
     print (url)
     return "<p>Hello World</p>"
