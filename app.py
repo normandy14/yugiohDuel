72b5f571040a16605cd7d04c8a8fff613092519c
normandy14 <sturdyprogrammer@gmail.com>
2023-03-25 20:26:30 -0400

    Rename pro to card

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for
+import csv
 import yugioh
 
 app = Flask(__name__)
@@ -7,35 +8,17 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-@app.route("/deck/<pro>")
-def deck(pro):
-    card = yugioh.get_card("Mirror Force") #Accepts both name and ID
-    if card.type != "Spell Card" and card.type != "Trap Card":
-        print(card.name) #Returns "The Wicked Dreadroot"
-        print(card.archetype) #Returns "Wicked God"
-        print(card.attack) #Returns "4000"
+@app.route("/deck/<card>")
+def deck(card):
+    card2 = yugioh.get_card("Mirror Force") #Accepts both name and ID
+    if card2.type != "Spell Card" and card2.type != "Trap Card":
+        print(card2.name) #Returns "The Wicked Dreadroot"
+        print(card2.archetype) #Returns "Wicked God"
+        print(card2.attack) #Returns "4000"
     else:
-        print(card.name)
-        print(card.description)
-    url = "https://static-7.studiobebop.net/ygo_data/card_images/{}.jpg".format(pro.capitalize())
+        print(card2.name)
+        print(card2.description)
+    url = "https://static-7.studiobebop.net/ygo_data/card_images/{}.jpg".format(card.capitalize())
     print (url)
     return "<p>Hello World</p>"
     
-
-'''
-@app.route("/yugioh/<card>")
-def card(card):
-    cardUppercase = card.capitalize()
-    url = "https://static-7.studiobebop.net/ygo_data/card_images/{}.jpg".format(cardUppercase)
-    print (url)
-    return url
-'''
-'''
-@app.route("/view/<card>")
-def view(card):
-    cardUppercase = card.capitalize()
-    cardUrl = "https://static-7.studiobebop.net/ygo_data/card_images/{}.jpg".format(cardUppercase)
-    return redirect(cardUrl)
-    # return cardUrl 
-    # https://static-7.studiobebop.net/ygo_data/card_images/Dark_Magician.jpg
-'''
