189ff851a4720acb6bf94fc32aad33a87f50fe0d
normandy14 <sturdyprogrammer@gmail.com>
2023-03-31 19:14:25 -0400

    Improve doBattlePhase with attack with and attack into clause. Implement first turn

--- app2.py
+++ app2.py
@@ -14,6 +14,7 @@ class Game:
         self.turn = 1
         self.zone1 = []
         self.zone2 = []
+        # self.firstTurn = True
     
     def loadHands(self):
         self.hand1 = self.deck1[0:5]
@@ -23,6 +24,7 @@ class Game:
         self.deck2 = self.deck2[5:]
     
     def draw(self):
+        print ("Draw time!")
         if self.turn == 1:
             self.hand1 = self.hand1 + self.deck1[:1]
             self.deck1 = self.deck1[1:]
@@ -35,6 +37,7 @@ class Game:
             self.turn = 2
         else:
             self.turn = 1
+        print ("Changed Turns!")
             
     # create monster zone1 and monster zone 2
     # set down selected card into monster zone 1 or monster zone2
@@ -47,8 +50,12 @@ class Game:
             while (userInput > lengthOfHand or userInput < 0):
                 print ("select from 0 to {}".format(lengthOfHand))
                 userInput = int(input("Select card to play: "))
-                print (self.hand1[userInput])
-                self.hand1.pop(userInput)
+                print ("selected card is : {}".format(self.hand1[userInput]))
+                card = self.hand1.pop(userInput)
+                self.zone1.append(card)
+                # print (self.zone1)
+                # self.doBattlePhase()
+          
         elif self.turn == 2:
             print (self.hand2)
             lengthOfHand = len(self.hand2) - 1
@@ -56,9 +63,47 @@ class Game:
             while (userInput > lengthOfHand or userInput < 0):
                 print ("select from 0 to {}".format(lengthOfHand))
                 userInput = int(input("Select card to play: "))
-                print (self.hand2[userInput])
-                self.hand2.pop(userInput)
-        
+                print ("selected card is : {}".format(self.hand2[userInput]))
+                card = self.hand2.pop(userInput)
+                self.zone2.append(card)
+                # print (self.zone2)
+                # self.doBattlePhase()
+                
+                
+    def doBattlePhase(self):
+        if self.turn == 1:
+            print ("Zone to attack with is {}".format(self.zone1))
+            lengthOfZone1 = len(self.zone1) - 1
+            print ("select 1 from {}".format(lengthOfZone1))
+            userInput = int(input("Select card to attack with: "))
+            cardAttack = self.zone1[userInput]
+            print ("card to attack with is {}".format(cardAttack))
+            
+            print ("Zone to attack into is {}".format(self.zone2))
+            lengthOfZone2 = len(self.zone2) - 1
+            print ("select 1 from {}".format(lengthOfZone2))
+            userInput2 = int(input("Select card to attack into: "))
+            cardAttackInto = self.zone2[userInput]
+            print ("card to attack into is {}".format(cardAttackInto))
+            
+       
+        elif self.turn == 2:
+            print ("zone to attack with is {}".format(self.zone2))
+            lengthOfZone2 = len(self.zone2) - 1
+            print ("select 1 from {}".format(lengthOfZone2))
+            userInput = int(input("Select card to attack with: "))
+            cardAttack = self.zone2[userInput]
+            print ("card to attack with is {}".format(cardAttack))
+            
+            print ("zone to attack into is {}".format(self.zone1))
+            lengthOfZone1 = len(self.zone1) - 1
+            print ("select from 0 to {}".format(lengthOfZone1))
+            userInput1 = int(input("Select card to attack into: "))
+            cardAttackInto = self.zone1[userInput]
+            # print (cardAttackInto)
+            print ("card to attack into is {}".format(cardAttackInto))
+            
+            
 # Functions for Game 
 
 def DeckSetup():
@@ -99,11 +144,16 @@ def main():
     # print (game.hand1)
     # print (game.hand2)
     
+    game.draw()
+    game.setCardToZone()
+    game.changeTurn()
+    # game.firstTurn = False
     while (1 == 1):
         game.draw()
         # print (game.hand1)
         game.setCardToZone()
         game.changeTurn()
+        game.doBattlePhase()
 
 if __name__ == "__main__":
     main()
